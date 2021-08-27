305a160c02abe382d6b8f1dfc5635579be85ab44
beingbravado <sharmagyanesh199@gmail.com>
2021-08-27 13:13:58 +0530

    game completed

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,4 @@
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
     HIDDEN: "hidden",
     MINE: 'mine',
     NUMBER: "number",
@@ -35,6 +35,59 @@ export function createBoard(boardSize, numberOfMines) {
     return board;
 }
 
+export function markTile(tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN && tile.status !== TILE_STATUSES.MARKED)
+        return;
+
+    if (tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN;
+    } else {
+        tile.status = TILE_STATUSES.MARKED;
+    }
+}
+
+export function revealTile(board, tile) {
+    // console.log(tile);
+    if (tile.status != TILE_STATUSES.HIDDEN)
+        return;
+
+    if (tile.mine) {
+        tile.status = TILE_STATUSES.MINE;
+        return;
+    }
+
+    tile.status = TILE_STATUSES.NUMBER;
+    const adjacentTiles = nearbyTiles(board, tile);
+    const mines = adjacentTiles.filter(t => t.mine);
+    if (mines.length === 0) {
+        adjacentTiles.forEach(revealTile.bind(null, board));
+    } else {
+        tile.element.textContent = mines.length;
+    }
+}
+
+export function checkWin(board) {
+    return board.every(row => {
+        return row.every(tile => {
+            return (
+                tile.status === TILE_STATUSES.NUMBER ||
+                (tile.mine && (
+                    tile.status === TILE_STATUSES.HIDDEN ||
+                    tile.status === TILE_STATUSES.MARKED
+                ))
+            )
+        })
+    })
+}
+
+export function checkLose(board) {
+    return board.some(row => {
+        return row.some(tile => {
+            return tile.status === TILE_STATUSES.MINE;
+        });
+    });
+}
+
 function getMinePositions(boardSize, numberOfMines) {
     const positions = [];
 
@@ -58,4 +111,18 @@ function positionMatch(a, b) {
 
 function randomNumber(size) {
     return Math.floor(Math.random() * size);
+}
+
+function nearbyTiles(board, { x, y }) {
+    const tiles = [];
+
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            const tile = board[x + xOffset]?.[y + yOffset];
+            if (tile)
+                tiles.push(tile);
+        }
+    }
+
+    return tiles;
 }
